08c5764f150b9d5f3a2f8ab4722b5124c040e8bf
Enter your name <Enter your email>
2022-01-27 14:06:59 +0400

    add getResource fetch func in PeoplePage

--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getApiResource } from '../../utils/network';
+import { API_PEOPLE } from '../../constants/api';
 
 const PeoplePage = () => {
+
+    const [people, setPeople] = useState(null)
+
+    const getResource = async (API_PEOPLE) => {
+        const res = await getApiResource(API_PEOPLE);
+
+        const peopleList = res.results.map(({ name, url }) => {
+            return {
+                name, url
+            }
+        })
+
+        setPeople(peopleList)
+        console.log(people, res);
+    }
+
+    useEffect(() => {
+        getResource(API_PEOPLE);
+        console.log('test')
+    }, [])
+
+    console.log(people);
     return (
-        <>
-            <h1>People page</h1>
-        </>
+        <nav>
+            <ul>
+                {
+                    people ? (
+                        people.map(({ name, url }) => <li key={name}>{name} {url}</li>)
+                    )
+                        : <h1>Loading...</h1>
+                }
+            </ul>
+        </nav>
     )
 };
 
